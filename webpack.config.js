01966301783fabb52f30b908ddc597998f779ebe
Daniel Gading <daniel.gading@agencychief.com>
2017-12-12 12:46:17 -0500

    Testing out webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'js')
+var ExtractCSS = new ExtractTextPlugin('[name].css');
+
+
+module.exports = [
+  {
+    entry: {
+      global: './src/js/index.js',
+      basic: './src/js/basic/index.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'js')
+    }
+  },
+  {
+    entry: {
+      global: './src/css/index.css',
+      basic: './src/css/basic/index.css',
+    },
+    output: {
+      filename: '[name].css',
+      path: path.resolve(__dirname, 'css')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractCSS.extract({
+             use: [
+              { loader: 'css-loader', options: { importLoaders: 1 } },
+              'postcss-loader'
+             ],
+          })
+        }
+      ]
+    },
+    plugins: [
+      ExtractCSS
+    ],
   }
-}
+];
